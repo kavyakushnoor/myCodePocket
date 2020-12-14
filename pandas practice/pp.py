2d2fdd5fc8c3f94c7df5c4c1cb2bf03ac0a7a17c
Kavya <k.kavya101@gmail.com>
2020-12-13 21:28:56 -0800

    more practice code

--- pandas practice/pp.py
+++ pandas practice/pp.py
@@ -9,8 +9,14 @@ for item in checkList:
     highest = df[item].max()
     lowest = df[item].min()
     stddev = df[item].std()
-    print (item, ' : ', '\n')
-    print ('mean: ', avg, 'max: ' , highest, 'min: ', lowest, 'stdev: ', stddev)
-    print ('\n')
-
-
+    print (item, ':', 'mean: ', avg, 'max: ' , highest, 'min: ', lowest, 'stdev: ', stddev)
+'''
+    new_df = df.agg({item: ['mean', 'std', 'max', 'min']})
+agg_df = pd.DataFrame()
+agg_df.columns = ['protein', 'fiber', 'vitamins', 'carbo']
+agg_df.rows = ['mean', 'std', 'max', 'min']
+    #print (new_df)
+    #agg_df=agg_df.join(new_df)
+    #agg_df=agg_df.rename(columns={item: item})
+print (agg_df)
+'''
